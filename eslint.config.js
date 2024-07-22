995f98a24a3a4d45832afec9a5e2af93edf58c10
Fraser Benjamin <fraser.benjamin@lego.com>
2024-07-22 15:40:42 +0100

    Added GitHub Workflow
    Signed-off-by: Fraser Benjamin <fraser.benjamin@lego.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: ["dist"],
+  },
 ];
